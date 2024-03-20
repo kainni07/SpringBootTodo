dd645dc3c0f8cb4fdfd94ac7ea556dd0481cb5fe
kainni07 <kainnicapri@gmail.com>
2024-03-20 18:44:50 +0900

    Update or delete records in a table

--- src/main/java/com/example/todo/controller/TodoListContoller.java
+++ src/main/java/com/example/todo/controller/TodoListContoller.java
@@ -6,9 +6,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import com.example.todo.repository.TodoRepository;
 import com.example.todo.service.TodoService;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.todo.entity.Todo;
 import com.example.todo.form.TodoData;
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class TodoListContoller {
   private final TodoRepository todoRepository;
   private final TodoService todoService;
+  private final HttpSession session;
   
   /*
    * ToDo一覧表示
@@ -42,6 +47,7 @@ public class TodoListContoller {
   public ModelAndView createTodo(ModelAndView mv) {
     mv.setViewName("todoForm");
     mv.addObject("todoData", new TodoData());
+    session.setAttribute("mode", "create");
     return mv;
   }
   
@@ -54,22 +60,23 @@ public class TodoListContoller {
    * @return
    */
   @PostMapping("/todo/create")
-  public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData,
+  public String createTodo(@ModelAttribute @Validated TodoData todoData,
                                 BindingResult result,
-                                ModelAndView mv) {
+                                Model model) {
       boolean isValid = todoService.isValid(todoData, result);
       
       if (!result.hasErrors() && isValid) {
         // エラーなし
         Todo todo = todoData.toEntity(); // エンティティクラスのオブジェクト
         todoRepository.saveAndFlush(todo); // テーブルに追加（INSERT文の発行）
-        return showTodoList(mv); // findAll()の代わりにshowTodoList()を呼び出す
+        // return showTodoList(mv); // findAll()の代わりにshowTodoList()を呼び出す
+        return "redirect:/todo";
         
       } else {
         // エラーあり
-        mv.setViewName("todoForm");
+        // mv.setViewName("todoForm");
         // mv.addObject("todoData", todoData); // @ModelAttrobiteが付与されたオブジェクトのため省略可
-        return mv;
+        return "todoForm";
       }
   }
   
@@ -81,4 +88,45 @@ public class TodoListContoller {
   public String cancel() {
       return "redirect:/todo";
   }
+  
+  /*
+   * ToDo入力(更新)画面表示
+   * 一覧画面でid押下時
+   */
+  @GetMapping("/todo/{id}") // {}URIテンプレート変数：URLパスから値を取り出し、@PathVariableでメソッド変数にセットする
+  public ModelAndView todoById(@PathVariable(name = "id") int id, ModelAndView mv) {
+    mv.setViewName("todoForm");
+    Todo todo = todoRepository.findById(id).get(); // Optional<Todo>型、値があればTodoオブジェクトを返し、なければnull
+    mv.addObject("todoData", todo);
+    session.setAttribute("mode", "update"); // 機能によって表示ボタンを変えるため、データを書き込む
+    return mv;
+  }
+  
+  /*
+   * 
+   * 入力画面で更新ボタン押下時
+   */
+  @PostMapping("/todo/update")
+  public String updateTodo(@ModelAttribute @Validated TodoData totoData, BindingResult result, ModelAndView mv) {
+    boolean isValid = todoService.isValid(totoData, result);
+    
+    if (!result.hasErrors() && isValid) {
+      // エラーなし
+      Todo todo = totoData.toEntity();
+      todoRepository.saveAndFlush(todo);
+      return "redirect:/todo";
+    } else {
+      // エラーあり
+      // model.addAttribute("todoData", totoData);
+      return "todoForm";
+    }
+  }
+  
+  @PostMapping("/todo/delete")
+  public String deleteTodo(@ModelAttribute TodoData todoData) {
+    todoRepository.deleteById(todoData.getId());
+    return "redirect:/todo";
+  }
+  
+  
 }
